c94ba4509d137f75e23e49833c9fab49b7e064b4
tabitha mugure <tarbiemugure@gmail.comgit config --global user.email tarbiemugure@gmail.com>
2022-09-08 15:51:39 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-Ã#include <stdio.h>
+#include <stdio.h>
 
 /**
 * main - print the string in the put function
@@ -8,3 +8,6 @@
 * Return: 0
 */
 int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
