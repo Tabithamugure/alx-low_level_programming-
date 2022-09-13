4121bdaa65d92dfaaee9aa629d27399ebd8b199d
tabitha mugure <tarbiemugure@gmail.comgit config --global user.email tarbiemugure@gmail.com>
2022-09-13 10:24:45 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-* main - prints putchar
-* Return: always 0
-*/
+ * main - print putchar
+ * Return: always 0
+ */
 int main(void)
 {
-    -putchar('_');
-    -putchar('p');
-    -putchar('u');
-    -putchar('t');
-    -putchat('c');
-    -putchar('h');
-    -putchar('a');
-    -putchar('r');
-    -putchar('\n');
-    return (0);
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        
+        return (0);
+
 }
