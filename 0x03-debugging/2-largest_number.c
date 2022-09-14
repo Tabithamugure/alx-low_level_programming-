56db2233692b14a0748d2851c96d62eac9880627
tabitha mugure <tarbiemugure@gmail.comgit config --global user.email tarbiemugure@gmail.com>
2022-09-14 06:27:28 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,13 @@
                                                                                                                                  
 int largest_number(int a, int b, int c)                                                                                          
 {                                                                                                                                
-int largest;                                                                                                                     
-if (a >= b && a >= c)                                                                                                            
-{                                                                                                                                
-largest = a;                                                                                                                     
-}                                                                                                                                
-else if (b >= a && b >= c)                                                                                                       
-{                                                                                                                                
+int largest;
+if (a >= b && a >= c)
+{
+largest = a;
+}
+else if (b >= a && b >= c)
+{
 largest = b;
 }
 else
