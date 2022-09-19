18a36aa64e03f199159e691e972fa01f7c452a76
tabitha mugure <tarbiemugure@gmail.comgit config --global user.email tarbiemugure@gmail.com>
2022-09-19 06:05:31 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,8 +5,8 @@
  *_puts - prints a string
  * @str: string to print
  *
- * Description: prints a string
- * On success: returns no error 
+ * Description: prints a string 
+ * On success: returns no error
  */
 
 void _puts(char *str)
